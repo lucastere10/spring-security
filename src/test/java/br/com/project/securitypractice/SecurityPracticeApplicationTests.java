2d9841edbbfc804d459eafedae8ab8e3dc50886f
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2024-01-22 08:54:49 -0300

    add: arquivos iniciais do projeto.

---
+++ src/test/java/br/com/project/securitypractice/SecurityPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.securitypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
